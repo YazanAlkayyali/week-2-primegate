cd73f49c8415896dbf1299c9feb4a43bf5321ab2
yazank <yazank105@gmail.com>
2026-09-15 15:47:42 +0300

    test: add retrieval tests

--- test_retrieval.py
+++ test_retrieval.py
@@ -1,5 +1,10 @@
 from src.week_2_primegate.retrieval import search_chapter
 
+results = search_chapter("what is the color of the sky")
+for r in results:
+    print(r[:200])
+    print("---")
+
 results = search_chapter("what is a cookie-cutter business plan")
 for r in results:
     print(r[:200])
