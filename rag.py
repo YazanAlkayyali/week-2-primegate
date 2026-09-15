555a45df029c1dad242137be313810238f93f11c
yazank <yazank105@gmail.com>
2026-09-15 08:55:49 +0300

    Remove tea_reviews dataset and collection, prep for entrepreneurship chapter dataset

--- rag.py
+++ rag.py
@@ -9,6 +9,30 @@ client = QdrantClient(
     api_key=os.getenv("QDRANT_API_KEY"),
 )
 
-print(client.get_collections())
-info = client.get_collection("tea_reviews")
-print(info)
+def chunk_text(text, chunk_size=200, overlap=50):
+    if len(text)<=chunk_size:
+        return [text]
+    chunks=[]
+    start=0
+    while start<len(text):
+        end= start + chunk_size
+        chunks.append(text[start:end])
+        start += chunk_size - overlap
+    return chunks
+
+COLLECTION="tea_reviews"
+MODEL="sentence-transformers/all-MiniLM-L6-v2"
+
+df = pd.read_csv("data/tea_reviews.csv")
+df = df.dropna(subset=["review_text"])
+
+
+#docs = [
+#    "Qdrant has a LangChain integration for chatbots.",
+#    "Qdrant has a LlamaIndex integration for agents.",
+#]
+#metadata = [
+#    {"source": "langchain-docs"},
+#    {"source": "llamaindex-docs"},
+#]
+#   ids = [42, 2]
